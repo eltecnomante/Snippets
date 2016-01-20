b41308c3fb5eaf598f9bc2a4d73c694de87a92f6
nachogrmns <ignacio.garcia@medianet.es>
2016-01-20 18:12:02 +0100

    ToDo project and resources

---
+++ backbone/js/routes.js
@@ -0,0 +1,14 @@
+define(function() {
+  return Backbone.Router.extend({
+    routes: {
+      'profile': 'shwoProfile'
+    },
+
+    initialize: function() {
+    },
+
+    showProfile: function() {
+
+    }
+  });
+});
