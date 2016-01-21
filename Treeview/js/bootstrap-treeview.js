7a93072f401428de54ccf092b5eb8a8f83454ae7
nachogrmns <ignacio.garcia@medianet.es>
2016-01-21 16:15:46 +0100

    TreeView con enlaces

--- Treeview/js/bootstrap-treeview.js
+++ Treeview/js/bootstrap-treeview.js
@@ -1,40 +1,49 @@
 var tree = [
   {
     text: "Parent 1",
+    href: "http://www.google.com",
     nodes: [
       {
         text: "Child 1",
         nodes: [
           {
-            text: "Grandchild 1"
+            text: "Grandchild 1",
+            href: "http://www.google.com",
           },
           {
-            text: "Grandchild 2"
+            text: "Grandchild 2",
+            href: "http://www.google.com",
           }
         ]
       },
       {
-        text: "Child 2"
+        text: "Child 2",
+        href: "http://www.google.com"
       }
     ]
   },
   {
-    text: "Parent 2"
+    text: "Parent 2",
+    href: "http://www.google.com",
   },
   {
-    text: "Parent 3"
+    text: "Parent 3",
+    href: "http://www.google.com",
   },
   {
-    text: "Parent 4"
+    text: "Parent 4",
+    href: "http://www.google.com",
   },
   {
-    text: "Parent 5"
+    text: "Parent 5",
+    href: "http://www.google.com",
   }
 ];
 
 $(document).on('ready',function(){
 
   $('#tree').treeview({
-    data: tree
+    data: tree,
+    enableLinks: true
   });
 });
