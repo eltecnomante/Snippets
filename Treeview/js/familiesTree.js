15a9ae4fde8fdc7f54f7c5fb5d50b22af9caf635
nachogrmns <ignacio.garcia@medianet.es>
2016-01-25 18:11:38 +0100

    Filtro de busqueda, carga por demanda del arbol

--- Treeview/js/familiesTree.js
+++ Treeview/js/familiesTree.js
@@ -1,30 +1,38 @@
-(function() {
+
   var origin = [];
   var transform = [];
   var tree = [];
-  var executeStep4 = function() {
+  var drawTree = function(level) {
   var final =  [];
+  transform = [];
     var  i = 0,
       j = 0,
       k = 0,
       l = 0;
     $.getJSON('js/families.json', {
       param1: 'value1'
-    }, function(json, textStatus) {
+    }).done(function(json, textStatus){
       origin = json;
 
       for (i; i < origin.Families_1.length; i++) {
-        transform.push({
-          'Identifier': origin.Families_1[i].Identifier,
-          'Text': origin.Families_1[i].Text,
-          'children': []
-        });
+        if(level>=1){
+          transform.push({
+            'id': origin.Families_1[i].Identifier,
+            'text': origin.Families_1[i].Text,
+            'children': []
+          });
+        }else{
+          transform.push({
+            'id': origin.Families_1[i].Identifier,
+            'text': origin.Families_1[i].Text
+          });
+        }
       }
 
       //Second level
       i = 0;
       j = 0;
-      for (i; i < origin.Families_2.length; i++) {
+      for (i; i < origin.Families_2.length && level>=2; i++) {
 
         var text = origin.Families_2[i].ParentId_1;
 
@@ -33,12 +41,19 @@
         j = 0;
         while (next && j < transform.length) {
 
-          if (transform[j].Identifier == text) {
-            transform[j].children.push({
-              'Identifier': origin.Families_2[i].Identifier,
-              'Text': origin.Families_2[i].Text,
-              'children': []
-            });
+          if (transform[j].id == text) {
+            if(level>=2){
+              transform[j].children.push({
+                'id': origin.Families_2[i].Identifier,
+                'text': origin.Families_2[i].Text,
+                'children': []
+              });
+            }else{
+              transform[j].children.push({
+                'id': origin.Families_2[i].Identifier,
+                'text': origin.Families_2[i].Text
+              });
+            }
             next = 0;
           }
           j++;
@@ -52,7 +67,7 @@
       k = 0;
       var nextGrandParent = 1;
       var next = 1;
-      for (i; i < origin.Families_3.length; i++) {
+      for (i; i < origin.Families_3.length && level>=3; i++) {
 
         var text = origin.Families_3[i].ParentId_1;
 
@@ -64,16 +79,23 @@
         next = 1;
         while (nextGrandParent && j < transform.length) {
 
-          if (transform[j].Identifier == family1) {
+          if (transform[j].id == family1) {
             nextGrandParent = 0;
             while (next && k < transform[j].children.length) {
 
-              if (transform[j].children[k].Identifier == family2) {
-                transform[j].children[k].children.push({
-                  'Identifier': origin.Families_3[i].Identifier,
-                  'Text': origin.Families_3[i].Text,
-                  'children': []
-                });
+              if (transform[j].children[k].id == family2) {
+                if(level>=3){
+                  transform[j].children[k].children.push({
+                    'id': origin.Families_3[i].Identifier,
+                    'text': origin.Families_3[i].Text,
+                    'children': []
+                  });
+                }else{
+                  transform[j].children[k].children.push({
+                    'id': origin.Families_3[i].Identifier,
+                    'text': origin.Families_3[i].Text
+                  });
+                }
                 next = 0;
               }
               k++;
@@ -92,7 +114,7 @@
       var nextGrandParent = 1;
       var nextG3Parent = 1;
       var next = 1;
-      for (i; i < origin.Families_4.length; i++) {
+      for (i; i < origin.Families_4.length && level>=4; i++) {
 
         var text = origin.Families_3[i].ParentId_1;
 
@@ -106,20 +128,20 @@
         nextG3Parent = 1;
         while (nextG3Parent && j < transform.length) {
 
-          if (transform[j].Identifier == family1) {
+          if (transform[j].id == family1) {
             nextG3Parent = 0;
             nextGrandParent = 1;
             while (nextGrandParent && k < transform[j].children.length) {
 
-              if (transform[j].children[k].Identifier == family2) {
+              if (transform[j].children[k].id == family2) {
                 nextGrandParent = 0;
                 next = 1;
                 while (next && l < transform[j].children[k].children.length) {
 
-                  if (transform[j].children[k].children[l].Identifier == family3) {
+                  if (transform[j].children[k].children[l].id == family3) {
                     transform[j].children[k].children[l].children.push({
-                      'Identifier': origin.Families_4[i].Identifier,
-                      'Text': origin.Families_4[i].Text
+                      'id': origin.Families_4[i].Identifier,
+                      'text': origin.Families_4[i].Text
                     });
                     next = 0;
                   //  console.log('inserting ' + j + ' ' + k + ' ' + l);
@@ -134,18 +156,67 @@
         }
       }
 
-      final.push({'text':'Root Node','id':1, children:transform});
-      tree= final;
+      final.push({'text':'Familias','id':1, children:transform});
+
+
+      $('#tree').jstree({
+        'core' : {
+          'data' : final
+        },
+        'plugins' : ['search']
+      });
     });
 
   };
 
-
+$(document).on('ready',function(){
   var start = new Date().getTime();
 
-  executeStep4();
+  drawTree(4);
 
 
+  $('input').change(function(){
+    if($(this).is(":checked")) {
+      $(this).prevAll().prop('checked','checked');
+      $(this).nextAll().removeAttr('checked');
+    }else{
+      $(this).nextAll().removeAttr('checked');
+    }
+    $('#firstlevel').prop('checked','checked');
+    $('#secondlevel').prop('checked','checked');
+
+  });
+
+  $('.loadTree').on('click',function(){
+    $parent=    $('#tree').parent();
+    $('#tree').remove();
+    $('<div id="tree"></div>').appendTo($parent);
+    drawTree($('input[type=checkbox]:checked').length);
+  });
+
+  $('#filter').on('keyup',function(){
+    $("#tree").jstree(true).search($("#filter").val());
+
+
+    $('li[aria-level=4]').each(function(){
+      if(!$(this).find('.jstree-search').length>0){
+        $(this).addClass('hidden');
+      }
+    });
+
+    $('li[aria-level=3]').each(function(){
+      if(!$(this).find('.jstree-search').length>0){
+        $(this).addClass('hidden');
+      }
+    });
+
+    $('li[aria-level=2]').each(function(){
+      if(!$(this).find('.jstree-search').length>0){        
+        $(this).addClass('hidden');
+      }
+    });
+
+
+  });
   console.log(new Date().getTime() - start);
-
-})();
+});
